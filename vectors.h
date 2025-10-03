e7685af277e8c0075dca786b5eaa279135a45cb3
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2025-10-03 19:47:28 +0700

    implement more vector functions

--- vectors.h
+++ vectors.h
@@ -1,6 +1,9 @@
 #ifndef _H_MATH_VECTORS_
 #define _H_MATH_VECTORS_
 
+#define RAD2DEG(x) ((x) * 57.295754f)
+#define DEG2RAD(x) ((x) * 0.0174533f)
+
 typedef struct vec2
 {
   union
@@ -56,5 +59,35 @@ bool operator==(const vec2& l, const vec2& r);
 bool operator==(const vec2& l, const vec2& r);
 bool operator!=(const vec2& l, const vec2& r);
 bool operator!=(const vec3& l, const vec3& r);
+
+float Dot(const vec2& l, const vec2& r);
+float Dot(const vec3& l, const vec3& r);
+
+
+float Magnitude(const vec2& v);
+float Magnitude(const vec3& v);
+
+float MagnitudeSq(const vec2& v);
+float MagnitudeSq(const vec3& v);
+
+void Normalzie(vec2& v);
+void Normalize(vec3& v);
+
+vec2 Normalized(const vec2& v);
+vec3 Normalized(const vec3& v);
+
+vec3 Cross(const vec3& l, const vec3& r);
+
+float Angle(const vec2& l, const vec2& r);
+float Angle(const vec3& l, const vec3& r);
+
+vec2 Project(const vec2& length, const vec2& direction);
+vec3 Project(const vec3& length, const vec3& direction);
+
+vec2 Perpendicular(const vec2& len, const vec2& dir);
+vec3 Perpendicular(const vec3& len, const vec3& dir);
+
+vec2 Reflection(const vec2& vec, const vec2& normal);
+vec3 Reflection(const vec3& vec, const vec2& normal);
 
 #endif
