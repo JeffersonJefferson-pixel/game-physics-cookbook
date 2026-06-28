7b355fcdc0465507f4e65b822b71a2d4b5dc55af
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2026-06-28 18:25:38 +0700

    add wndproc and opengl context function in main win

--- main-win32.cpp
+++ main-win32.cpp
@@ -1,10 +1,10 @@
 #if _DEBUG
-    #define _CRTDBG_MAP_ALLOC
-    #include <stdlib.h>
-    #include <crtdbg.h>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
-    #include <float.h>
-    unsigned int fp_control_state = _controlfp(_EM_INEXACT, _MCW_EM);
+#include <float.h>
+unsigned int fp_control_state = _controlfp(_EM_INEXACT, _MCW_EM);
 #endif
 
 #define MAIN_WIN_32_WINDOW_CLASS L"Win32GLSandbox"
@@ -28,26 +28,43 @@
 #include "imgui/imgui_implementation.h"
 #include "imgui/ImGuizmo.h"
 
-bool CheckIfAlreadyRunning(IWindow* pWindowInstance);
+RECT windowRect;
+RECT clientRect;
+RECT borderRect;
 
-int main(int argc, const char** argv) {
-    IWindow* pWindowInstance = IWindow::GetInstance();
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTSR szCmdLine, int iCmdShow);
+bool CheckIfAlreadyRunning(IWindow *pWindowInstance);
+HWND MakeNewWindow(IWindow *pWindowInstance, HInstance hInstance, LPSTR szCmdLine);
+HWND OpenGLBindContext(HDC hdc);
+void OpenGLUnbindContext(HWND hwnd, HDC hdc, HGLRC hglrc);
+void UpdateFullscreen(IWindow *pWindowInstance, HWND hwnd, HDC hdc);
+void SetDisplayMode(int width, int height, int bpp, int refreshrate);
 
-    if (CheckIfAlreadyRunning(pWindowInstance)) {
+
+int main(int argc, const char **argv)
+{
+    IWindow *pWindowInstance = IWindow::GetInstance();
+
+    if (CheckIfAlreadyRunning(pWindowInstance))
+    {
         std::cout << "Something went wrong\n";
         getch();
     }
-    else if (CheckIfAlreadyRunning(pWindowInstance)) {
+    else if (CheckIfAlreadyRunning(pWindowInstance))
+    {
         std::cout << "Error, only on instance\n";
         getch();
     }
-    else {
+    else
+    {
         WinMain(GetModuleHandle(NULL), NULL, GetCommandLineA(), SW_SHOWDEFAULT);
     }
     return 0;
 }
 
-HWND MakeNewWindow(IWindow* pWindowInstance, HINSTANCE hInstance, LPSTR szCmdLine) {
+HWND MakeNewWindow(IWindow *pWindowInstance, HINSTANCE hInstance, LPSTR szCmdLine)
+{
     std::wstring MAIN_WIN32_WINDOW_NAME;
     pWindowInstance->GetAndResetTitleDirtyFlag();
     std::string title(pWindowInstance->GetTitle());
@@ -55,19 +72,22 @@ HWND MakeNewWindow(IWindow* pWindowInstance, HINSTANCE hInstance, LPSTR szCmdLin
 
     return CreateWindowEx(0, MAIN_WIN_32_WINDOW_CLASS, MAIN_WIN32_WINDOW_NAME.c_str(),
 #ifdef ENABLE_RESIZE
-        NORMAL_STYLE
+                          NORMAL_STYLE
 #else
-        NO_RESIZE_STYLE
+                          NO_RESIZE_STYLE
 #endif
-        , windowRect.left, windowRect.top, 
-        windowRect.right - windowRect.left, windowRect.bottom - windowRect.top,
-        NULL, NULL, hInstance, szCmdLine);
+                          ,
+                          windowRect.left, windowRect.top,
+                          windowRect.right - windowRect.left, windowRect.bottom - windowRect.top,
+                          NULL, NULL, hInstance, szCmdLine);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow) {
-    IWindow* pWindowInstance = IWindow::GetInstance();
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
+{
+    IWindow *pWindowInstance = IWindow::GetInstance();
 
-    if (CheckIfAlreadyRunning(pWindowInstance)) {
+    if (CheckIfAlreadyRunning(pWindowInstance))
+    {
         MessageBox(NULL, L"Only one instance", NULL, NULL);
         return FALSE;
     }
@@ -93,29 +113,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
     wndclass.lpszClassName = MAIN_WIN_32_WINDOW_CLASS;
     RegisterClassEx(&wndclass);
 
-    SetRect(&windowRect, (GetSystemMetrics(SM_CXSCREEN) / 2) - (width / 2), 
-        (GetSystemMetrics(SM_CYSCREEN) / 2) - (height / 2), 
-        (GetSystemMetrics(SM_CXSCREEN) / 2) + (width / 2), 
-        (GetSystemMetrics(SM_CYSCREEN) / 2) + (height / 2));
+    SetRect(&windowRect, (GetSystemMetrics(SM_CXSCREEN) / 2) - (width / 2),
+            (GetSystemMetrics(SM_CYSCREEN) / 2) - (height / 2),
+            (GetSystemMetrics(SM_CXSCREEN) / 2) + (width / 2),
+            (GetSystemMetrics(SM_CYSCREEN) / 2) + (height / 2));
 
-    AdjustWindowRect(&windowRect, 
-    #ifdef ENABLE_RESIZE
-        NORMAL_STYLE
-    #else
-        NO_RESIZE_STYLE
-    #endif
-        , FALSE, 0);
-    
+    AdjustWindowRect(&windowRect,
+#ifdef ENABLE_RESIZE
+                     NORMAL_STYLE
+#else
+                     NO_RESIZE_STYLE
+#endif
+                     ,
+                     FALSE, 0);
+
     HWND hwnd = MakeNewWindow(pWindowInstance, hInstance, szCmdLine);
 
     HDC hdc = GetDC(hwnd);
     HGLRC hglrc = OpenGLBindContext(hdc);
 
-    if (!gladLoadGL()) {
+    if (!gladLoadGL())
+    {
         std::cout << "Could not instantiate GLAD OpenGL 2.1 context\n";
         exit(-1);
     }
-    else if (GLVersion.major < 2) {
+    else if (GLVersion.major < 2)
+    {
         std::cout << "Your system  doesn't support OpenGL >= 2!\n";
         return -1;
     }
@@ -128,9 +151,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
     pWindowInstance->OnInitialize();
 
     bool fullscreen = pWindowInstance->GetFullScreen();
-    if (fullscreen) {
+    if (fullscreen)
+    {
         UpdateFullscreen(pWindowInstance, hwnd, hdc);
-    } else {
+    }
+    else
+    {
         pWindowInstance->OnResize(width, height);
     }
 
@@ -149,9 +175,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
 
     pWindowInstance->MarkAsShown();
 
-    while (!pWindowInstance->GetQuitFlag()) {
-        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            if (msg.message == WM_QUIT) {
+    while (!pWindowInstance->GetQuitFlag())
+    {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
                 break;
             }
             TranslateMessage(&msg);
@@ -161,28 +190,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
         ImGuizmo::BeginFrame();
 
         // change screen title
-        if (pWindowInstance->GetAndResetTitleDirtyFlag()) {
-            std::wstring MAIN_WIN32_WINDOW-NAME;
+        if (pWindowInstance->GetAndResetTitleDirtyFlag())
+        {
+            std::wstring MAIN_WIN32_WINDOW - NAME;
             std::string title(pWindowInstance->GetTitle());
             MAIN_WIN32_WINDOW_NAME = std::wstring(title.begin(), title.end());
             SetWindowText(hwnd, MAIN_WIN32_WINDOW_NAME.c_str());
         }
 
         // toggle full screen
-        if (fullscreen != pWindowInstance->GetFullScreen()) {
+        if (fullscreen != pWindowInstance->GetFullScreen())
+        {
             UpdateFullscreen(pWindowInstance, hwnd, hdc);
             fullscreen = pWindowInstance->GetFullScreen();
         }
 
-        if (!fullscreen) {
+        if (!fullscreen)
+        {
             int windowWidth = borderRect.right - borderRect.left;
             int windowHeight = borderRect.bottom - borderRect.top;
             int clientWidth = clientRect.right - clientRect.left;
-            int clientHeight  = clientRect.bottom - clientRect.top;
+            int clientHeight = clientRect.bottom - clientRect.top;
             int borderWidth = windowWidth - clientWidth;
             int borderHeight = windowHeight - clientHeight;
 
-            if (clientWidth != pWindowInstance->GetWidth() || clientHeight != pWindowInstance->GetHeight()) {
+            if (clientWidth != pWindowInstance->GetWidth() || clientHeight != pWindowInstance->GetHeight())
+            {
                 SetWindowPos(hwnd, 0, windowRect.left, windowRect.top, pWindowInstance->GetWidth() + borderWidth, pWindowInstance->GetHeight() + borderHeight, SWP_NOZORDER);
             }
         }
@@ -194,7 +227,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
         pWindowInstance->OnUpdate(deltaTime);
 
         fixed_ellapsed += deltaTime;
-        while (fixed_ellapsed > fixed_millis) {
+        while (fixed_ellapsed > fixed_millis)
+        {
             pWindowInstance->OnFixedUpdate(fixed_millis);
             fixed_ellapsed -= fixed_millis;
         }
@@ -206,7 +240,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
         int SKIP_TICKS = 1000 / pWindowInstance->GetTargetFPS();
         next_game_tick += SKIP_TICKS;
         sleep_time = next_game_tick - GetTickCount();
-        if (sleep_time >= 0) {
+        if (sleep_time >= 0)
+        {
             Sleep(sleep_time);
         }
     }
@@ -222,4 +257,239 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
 #endif
 
     return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    ImGui_Implementation_WndProcHandler(hwnd, iMsg, wParam, lParam);
+
+    iWindow *pWindowInstance = IWindow::GetInstance();
+    int width, height;
+
+    static bool shiftDown = false;
+    static bool capsOn = false;
+
+    switch (iMsg)
+    {
+    // window lifecycle events
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        pWindowInstance->Close();
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    // resize event
+    case WM_SIZE:
+        width = LOWORD(lParam);
+        height = HIWORD(lParam);
+
+        GetClientRect(hwnd, &clientRect);
+        GetWindowRect(hwnd, &borderRect);
+        pWindowInstance->Resize(width, height);
+        if (pWindowInstance->WasWindowShown())
+        {
+            pWindowInstance->OnResize(width, height);
+        }
+        break;
+    // mouse move event
+    case WM_MOUSEMOVE:
+        if (!ImGui::Implementation_HasMouse())
+        {
+            pWindowInstance->OnMouseMove(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+        }
+        break;
+    case WM_LBUTTONDOWN:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsUsing() && !ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseDown(MOUSE_LEFT);
+            }
+        }
+        break;
+    case WM_LBUTTONUP:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsUsing() && !ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseUp(MOUSE_LEFT);
+            }
+        }
+        break;
+    case WM_RBUTTONDOWN:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsUsing() && !ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseDown(MOUSE_RIGHT);
+            }
+        }
+        break;
+    case WM_RBUTTONUP:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsUsing() && !ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseUp(MOUSE_RIGHT);
+            }
+        }
+        break;
+    case WM_MBUTTONDOWN:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsUsing() && !ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseDown(MOUSE_MIDDLE);
+            }
+        }
+        break;
+    case WM_MBUTTONUP:
+        if (!ImGui_Implementation_HasMouse())
+        {
+            if (!ImGuizmo::IsOver())
+            {
+                pWindowInstance->OnMouseMove(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+            }
+            if (!ImGuizmo::IsUsing())
+            {
+                pWindowInstance->OnMouseUp(MOUSE_MIDDLE);
+            }
+        }
+        break;
+    // keyboard events
+    case WM_SYSKEYUP:
+        break;
+    case WM_SYSKEYDOWN:
+        break;
+    case WM_KEYDOWN:
+        if (wParam == VK_SHIFT || wParam == VK_LSHIFT || wParam == VK_RSHIFT)
+        {
+            shiftDown = true;
+        }
+        if (wParam == VK_CAPITAL)
+        {
+            capsOn = !capsOn;
+        }
+        if (!ImGui_Implementation_HasKeyboard())
+        {
+            pWindowInstance->OnKeyDown(wParamToKeydef(wParam, shiftDown ^ capsOn));
+        }
+        break;
+    case WM_KEYUP:
+        if (wParam == VK_SHIFT || wParam == VK_LSHIFT || wParam == VK_RSHIFT)
+        {
+            shiftDown = false;
+        }
+        if (!ImGui_Implementation_HasKeyboard())
+        {
+            pWindowInstance->OnKeyUp(wParamToKeydef(wParam, shiftDown ^ capsOn));
+        }
+        break;
+    }
+
+    return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+bool CheckIfAlreadyRunning(IWindow *pWindowInstance)
+{
+    std::wstring MAIN_WIN32_WINDOW_NAME;
+    std::string title(pWindowInstance->GetTitle());
+    MAIN_WIN32_WINDOW_NAME = std::wstring(title.begin(), title.end());
+
+    HWND hWnd = FindWindow(MIN_WIN_32_WINDOW_CLASS, MAIN_WIN32_WINDOW_NAME.c_str());
+
+    if (hWnd)
+    {
+        if (IsIconic(hWnd))
+        {
+            ShowWindow(hWnd, SW_RESTORE());
+        }
+        SetForegroundWindow(hWnd);
+        return true;
+    }
+    return false;
+}
+
+HGLRC OpenGLBindContext(HDC hdc)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 32;
+    pfd.cStencilBits = 8;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixelFormat, &pfd);
+
+    HGLRC context = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, context);
+    return context;
+}
+
+void OpenGLUnbindContext(HWND hwnd, HDC hdc, HGLRLC hglrc)
+{
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(hglrc);
+    ReleaseDC(hwnd, hdc);
+}
+
+void UpdateFullscreen(IWindow *pWindowInstance, HWND hwnd, HDC hdc)
+{
+    if (pWindowInstance->GetFullScreen())
+    {
+        GetWindowRect(hwnd, &windowRect);
+        SetDisplayMode(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), GetDeviceCaps(hdc, BITSPIXEL), GetDeviceCaps(hdc, VREFRESH));
+        SetWindowLongPtr(hwnd, GWL_STYLE, WSPOPUP);
+        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
+        SetWindowPos(hwnd, 0, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN) + 1, SWP_NOZORDER);
+        ShowCursor(FALSE);
+    }
+    else
+    {
+        SetDisplayMode(0, 0, 0, 0);
+        SetWindowLongPtr(hwnd, GWL_STYLE,
+#ifdef ENABLE_RESIZE
+                         NORMAL_STYLE
+#else
+                         NO_RESIZE_STYLE
+#endif
+        );
+        SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
+		int iWindowWidth = windowRect.right - windowRect.left;
+		int iWindowHeight = windowRect.bottom - windowRect.top;
+		SetWindowPos(hwnd, 0, windowRect.left, windowRect.top, iWindowWidth, iWindowHeight, SWP_NOZORDER);
+		ShowCursor(TRUE);
+    }
+
+    GetClientRect(hwnd, &clientRect);
+    GetWindowRect(hwnd, &borderRect);
+}
+
+void SetDisplayMode(int width, int height, int bpp, int refreshRate) {
+	if (width == 0 && height == 0 && bpp == 0 && refreshRate == 0) {
+		ChangeDisplaySettings(NULL, 0);
+		return;
+	}
+
+	DEVMODE dm;
+	ZeroMemory(&dm, sizeof(DEVMODE));
+	dm.dmSize = sizeof(DEVMODE);
+
+	int i = 0;
+	while (EnumDisplaySettings(NULL, i++, &dm)) {
+		if (dm.dmPelsWidth == width && dm.dmPelsHeight == height &&
+			dm.dmBitsPerPel == bpp && dm.dmDisplayFrequency == refreshRate) {
+			if (ChangeDisplaySettings(&dm, CDS_TEST) == DISP_CHANGE_SUCCESSFUL) {
+				ChangeDisplaySettings(&dm, CDS_FULLSCREEN);
+				return;
+			}
+		}
+	}
 }
